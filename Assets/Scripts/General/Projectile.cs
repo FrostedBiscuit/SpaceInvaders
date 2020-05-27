a0740ce62272b9add71a126ab7997f9a5dffb0c2
Gasper Kaucic <gkaucic@descartes.com>
2020-05-27 10:51:19 +0200

    Initial commit

---
+++ Assets/Scripts/General/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float YKill = 50f;
+
+    [SerializeField]
+    private Vector2 StartVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(StartVelocity);
+    }
+
+    void Update()
+    {
+        if (transform.position.y > YKill)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float MovementSpeed = 10f;
+    [SerializeField]
+    private float MaxMovementToOneSide = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > -MaxMovementToOneSide && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += (Vector3)(Vector2.left * Time.deltaTime * MovementSpeed);
+        }
+        else if (transform.position.x < MaxMovementToOneSide && Input.GetKey(KeyCode.RightArrow)) {
+
+            transform.position += (Vector3)(Vector2.right * Time.deltaTime * MovementSpeed);
+        }
+    }
+}
